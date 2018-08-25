cc3a729fa113bb3cf46ed6c1d59a7b7835327f78
NOYNOYN <42329540+NOYNOYN@users.noreply.github.com>
2018-08-25 15:56:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,43 +1,19 @@
+
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-})
-}
-});
+var prefix = "-"
 
 
-
-
-client.on("message", (message) => {
-
-    if (isCommand(message, "-new")) {
+ 
+       client.on("message", (message) => {
+    
+                        if (message.content.startsWith(prefix + "new")) {
         const reason = message.content.split(" ").slice(1).join(" ");
-        if (!message.guild.roles.exists("name", "◆ Host Support")) return message.channel.send(`لا يحتوي هذا الخادم على دور \ 'دعم مضيف \ "، وبالتالي لن يتم فتح التذكرة. \ n إذا كنت مسؤولاً ، فأنشئ واحدة بهذا الاسم بالضبط واعطها للمستخدمين الذين سيكون بمقدورهم رؤية تذاكر.`);
-        if (message.guild.channels.exists("name", "تذكرة" + message.author.id)) return message.channel.send(`ستفتح تذكرة جديدة`);
-        message.guild.createChannel(`تذكرة${message.author.id}`, "text").then(c => {
-            let role = message.guild.roles.find("name", "◆ Host Support");
+        if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
+        if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`You already have a ticket open.`);
+        message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
+            let role = message.guild.roles.find("name", "Support Team");
             let role2 = message.guild.roles.find("name", "@everyone");
             c.overwritePermissions(role, {
                 SEND_MESSAGES: true,
@@ -51,24 +27,23 @@ client.on("message", (message) => {
                 SEND_MESSAGES: true,
                 READ_MESSAGES: true
             });
-            message.channel.send(`:white_check_mark: تذكرتك تم فتحها #${c.name}.`);
+            message.channel.send(`تم أنشاء التذكره , #${c.name}.`);
             const embed = new Discord.RichEmbed()
                 .setColor(0xCF40FA)
-                .addField(`Hey ${message.author.username}!`, `يرجى محاولة شرح سبب فتح هذه التذكرة بأكبر قدر ممكن من التفاصيل. سيكون لدينا ** فريق الدعم ** قريباً لمساعدتك.`)
+                .addField(` ${message.author.username}!`, ` *** شكرًا لك على التواصل مع فريق الدعم! سنرد عليك في أقرب وقت ممكن.. *** `)
                 .setTimestamp();
             c.send({
                 embed: embed
             });
-        }).catch(console.error);
+        }).catch(console.error); 
     }
 
+                        if (message.content.startsWith(prefix + "close")) {
+        if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`لا يمكنك استخدام أمر الإغلاق خارج روم التذكره.`);
 
-    if (isCommand(message, "-close")) {
-        if (!message.channel.name.startsWith(`تذكرة`)) return message.channel.send(`لا يمكنك استخدام أمر الإغلاق خارج قناة التذاكر.`);
-
-        message.channel.send(`هل أنت واثق؟ بعد التأكيد ، لا يمكنك عكس هذا الإجراء! \ n للتأكيد ، اكتب \ "-confirm \`. سوف ينتهي المهلة خلال 10 ثوانٍ ويتم إلغاؤها.`)
+        message.channel.send(` ***هل أنت متأكد من إغلآق التذكرة ؟, Type $confirm to close the ticket., لديك 10 ثوآني للتأكيد .***`)
             .then((m) => {
-                message.channel.awaitMessages(response => response.content === '-confirm', {
+                message.channel.awaitMessages(response => response.content === '$confirm', {
                         max: 1,
                         time: 10000,
                         errors: ['time'],
@@ -77,7 +52,7 @@ client.on("message", (message) => {
                         message.channel.delete();
                     })
                     .catch(() => {
-                        m.edit('انتهى إغلاق التذاكر ، لم يتم إغلاق التذكرة.').then(m2 => {
+                        m.edit('أنتهى الوقت لن يتم أغلاق لتذكره').then(m2 => {
                             m2.delete();
                         }, 3000);
                     });
@@ -85,5 +60,4 @@ client.on("message", (message) => {
     }
 
 });
-
 client.login(process.env.BOT_TOKEN);
